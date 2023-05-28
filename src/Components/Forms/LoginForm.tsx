e8d1ce7f372f50c0d515df42d4a85178161a996d
Arjun Biju <arjunan8080@gmail.com>
2023-05-28 01:53:57 -0400

    build fix

--- src/Components/Forms/LoginForm.tsx
+++ src/Components/Forms/LoginForm.tsx
@@ -75,7 +75,6 @@ export const Login: React.FC<LoginProps> = ({ toggleForm }) => {
                             id="password"
                             placeholder="••••••••"
                             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            required
                             onChange={handleChange}
                         />
                         {errors.password && <p className="mt-2 text-sm text-red-600 dark:text-red-500"><span className="font-medium">Oops!</span>{errors.password}</p>
