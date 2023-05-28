0059944b26786587f99d89516712dfedf5b88234
Arjun Biju <arjunan8080@gmail.com>
2023-05-28 01:58:54 -0400

    removed html validations

--- src/Components/Forms/RegisterForm.tsx
+++ src/Components/Forms/RegisterForm.tsx
@@ -55,7 +55,7 @@ export const RegisterForm: React.FC<RegisterProps> = ({ toggleForm }) => {
                             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                             placeholder="name@email.com"
                             onChange={handleChange}
-                            required
+                            
                         />
                         {errors.email && <p className="mt-2 text-sm text-red-600 dark:text-red-500"><span className="font-medium">Oops!</span> Invalid Email!</p>
                         }                    </div>
@@ -72,7 +72,6 @@ export const RegisterForm: React.FC<RegisterProps> = ({ toggleForm }) => {
                             id="password"
                             placeholder="••••••••"
                             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            required
                             onChange={handleChange}
                         />
                         {errors.password && <p className="mt-2 text-sm text-red-600 dark:text-red-500"><span className="font-medium">Oops!</span> {errors.password}</p>
@@ -91,7 +90,6 @@ export const RegisterForm: React.FC<RegisterProps> = ({ toggleForm }) => {
                             id="confirm_password"
                             placeholder="••••••••"
                             className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            required
                             onChange={handleChange}
                         />
                         {errors["confirm_password"] && <p className="mt-2 text-sm text-red-600 dark:text-red-500"><span className="font-medium">Oops!</span> {errors["confirm_password"]}!</p>
